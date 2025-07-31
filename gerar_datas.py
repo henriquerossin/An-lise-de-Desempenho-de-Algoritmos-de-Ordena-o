ba9fcf825fd6ab34c023244e27ddebef1f18afc6
Henrique Rossin <pro.henriquerossin@gmail.com>
2025-07-31 13:11:10 -0300

    alteração no funcionamento

--- gerar_datas.py
+++ gerar_datas.py
@@ -11,6 +11,6 @@ def gerar_datas_aleatorias(n):
         data_base = datetime(2020, 1, 1)
         data_aleatoria = data_base + \
             timedelta(days=random.randint(0, 365*3),
-                      seconds=random.randint(0, 86400))
+                    seconds=random.randint(0, 86400))
         datas.append(data_aleatoria.strftime('%Y-%m-%d %H:%M:%S'))
     return datas
